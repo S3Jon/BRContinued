3ba2d9c632126418135d785fcf414ba31d03c4ab
Jon <sanchezeguiajon@gmail.com>
2024-06-20 02:57:42 +0200

    [fix] arreglada height list_forbidden

--- app/views/list_derivadas/list_forbidden.php
+++ app/views/list_derivadas/list_forbidden.php
@@ -1,7 +1,7 @@
 
 
 <!-- Mensaje de que no tiene acceso a esa pagina -->
-<div class="w-3/4 mx-auto min-h-sc">
+<div class="w-3/4 mx-auto min-h-screen">
 	<div class="bg-white shadow-md rounded-lg max-w-sm w-full">
 		<div class="p-4">
 			<h1 class="text-lg font-semibold text-gray-900">No tienes acceso a esta lista</h1>
